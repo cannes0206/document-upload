bced295ca505b03073de52a30f89b7b91e844ceb
Cherry Santos <cherry.santos@filamsoftware.com>
2022-01-31 15:43:20 +0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MatDialogModule} from '@angular/material/dialog';
 
 import { AppComponent } from './app.component';
+import { ContractListComponent } from './contract/contract-list.component';
+import { ContractDetailsComponent } from './contract/contract-details/contract-details.component';
+import { DocumentThumbnailComponent } from './contract/document-thumbnail/document-thumbnail.component';
+import { DocumentUploadModalComponent } from './contract/document-upload-modal/document-upload-modal.component';
+import { DeleteConfirmationModalComponent } from './contract/delete-confirmation-modal/delete-confirmation-modal.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SafeUrlPipe } from './shared/pipes/safe-url.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContractListComponent,
+    ContractDetailsComponent,
+    DocumentThumbnailComponent,
+    DocumentUploadModalComponent,
+    DeleteConfirmationModalComponent,
+    SafeUrlPipe
   ],
   imports: [
-    BrowserModule
+  BrowserModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    ReactiveFormsModule
   ],
   providers: [],
+  entryComponents: [DocumentUploadModalComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
